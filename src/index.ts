248e29d75619f13a28b93bbf7baab96d5f94f773
Ajay Maurya <ajaymaurya6798@gmail.com>
2024-01-03 17:58:29 +0530

    updated path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { ISegments, ISpinWheelProps } from "./Components/SpinWheel.interface"
-import SpinWheel from "./Components/SpinWheel"
+import { ISegments, ISpinWheelProps } from "./components/SpinWheel.interface"
+import SpinWheel from "./components/SpinWheel"
 
 export type { ISegments, ISpinWheelProps }
 export { SpinWheel }
